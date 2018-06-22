8d0cf1492c58a6e4a7bcf5ecb27fa8c86b454201
Alexandros Dorodoulis <aldor94@gmail.com>
2018-06-22 14:12:16 +0000

    chore(tools): Update docs release script

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -1,31 +1,32 @@
-const { cd, exec, echo, touch } = require("shelljs")
-const { readFileSync } = require("fs")
-const url = require("url")
+const { cd, exec, echo, touch } = require("shelljs");
+const { readFileSync } = require("fs");
+const url = require("url");
 
-let repoUrl
-let pkg = JSON.parse(readFileSync("package.json") as any)
+let repoUrl;
+let pkg = JSON.parse(readFileSync("package.json") as any);
 if (typeof pkg.repository === "object") {
   if (!pkg.repository.hasOwnProperty("url")) {
-    throw new Error("URL does not exist in repository section")
+    throw new Error("URL does not exist in repository section");
   }
-  repoUrl = pkg.repository.url
+  repoUrl = pkg.repository.url;
 } else {
-  repoUrl = pkg.repository
+  repoUrl = pkg.repository;
 }
 
-let parsedUrl = url.parse(repoUrl)
-let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
-let ghToken = process.env.GH_TOKEN
+const parsedUrl = url.parse(repoUrl);
+const repository = (parsedUrl.host || "") + (parsedUrl.path || "");
+const ghToken = process.env.GH_TOKEN;
+const version = pkg.version;
 
-echo("Deploying docs!!!")
-cd("docs")
-touch(".nojekyll")
-exec("git init")
-exec("git add .")
-exec('git config user.name "Duskfall"')
-exec('git config user.email "cyber_andrew6@hotmail.com"')
-exec('git commit -m "docs(docs): update gh-pages"')
+echo("Deploying docs!!!");
+cd("docs");
+touch(".nojekyll");
+exec("git init");
+exec("git add .");
+exec('git config user.name "KissMyButton Bot"');
+exec('git config user.email "opensource@kissmybutton.gr"');
+exec(`git commit -m "docs(docs): Publish docs for version ${version}"`);
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
-)
-echo("Docs deployed!!")
+);
+echo("Docs deployed!!");
